540ffe33032b7669713c4b7afde27707a61a923b
Pengpa0012 <peng.ilikha@gmail.com>
2023-01-07 13:14:26 +0800

    update server connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const cors = require("cors")
-const mongoose = require("mongoose")
+const connectDB = require("./config/db");
 
 const dotenv = require('dotenv');
 dotenv.config();
-console.log(`Your port is ${process.env.DBNAME}`);
+connectDB();
 
 const app = express()
 
@@ -16,20 +16,8 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 // Routes
-const usersRoutes = require("./routes/users")
+const usersRoutes = require("./routes/users");
 app.use("/users", usersRoutes) 
-
-// MongoDB Connection
-
-const dbConfig = process.env.ENPOINT
-const dbName = process.env.DBNAME
- 
-mongoose.set('strictQuery', false)
-
-mongoose.connect(`${dbConfig}/${dbName}`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
 
 const port = process.env.PORT || 3001
 
